7a51053284b2e03cb232c7494cd805fe20f5fcc3
David Viloria Ortega <david.viloria@citi.com.mx>
2022-06-30 15:02:26 -0500

    Caracteres máximos textField

--- BeerApp/LoginModule/LoginViewController.swift
+++ BeerApp/LoginModule/LoginViewController.swift
@@ -20,12 +20,45 @@ class LoginViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.addTarget(self, action: #selector(self.login(_:)), for: .touchUpInside)
+        passwordTextfield.isSecureTextEntry = true
+        configureTexfields()
+        configureTapGesture()
     }
     
     @objc func login(_ sender: UIButton){
         presenter?.goToListBeer()
+        view.endEditing(true)
     }
     
+    private func configureTexfields(){
+        usernameTextfield.delegate = self
+        passwordTextfield.delegate = self
+    }
+    
+    private func configureTapGesture(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func handleTap(_ sender: UIButton!){
+        view.endEditing(true)
+    }
+    
+}
+
+extension LoginViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxCharacters = 8
+        let currentString = (textField.text ?? "") as NSString
+        let newString: NSString = currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxCharacters
+    }
 }
 
 
